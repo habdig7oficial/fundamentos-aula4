d6226c2246ce777fec9e10b0aa9a03b5ffce1a18
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-08-20 21:15:39 -0300

    helo

---
+++ ex5.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+class Pessoa {
+
+    public:
+        string nome;
+        int idade;
+        string rg;
+        string cpf;
+        float renda;
+};
+
+
+int main(){
+
+    return 0;
+}
